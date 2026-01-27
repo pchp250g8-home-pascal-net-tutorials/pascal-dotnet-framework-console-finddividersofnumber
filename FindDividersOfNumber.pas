a04d0cf26ea2c4977d44f43f834071d3b0650975
Alexander <pchp250g8@tutamail.com>
2026-01-27 13:17:51 +0300

    Created Project FindDividersOfNumber

---
+++ FindDividersOfNumber.pas
@@ -0,0 +1,17 @@
+﻿uses
+  System;
+begin
+  Console.WriteLine('Input an integer positive number');
+	var ulNumber : Uint64;
+	UInt64.TryParse(Console.ReadLine(), ulNumber);
+	Console.WriteLine('The number {0} has the folowing dividers:');
+	for var i : Uint64 := 1 To ulNumber do
+  begin
+		if ulNumber mod i = 0 then
+    begin
+			Console.Write('{0} ', i);
+		end;
+	end;
+	Console.WriteLine();
+	Console.Read()
+end.
